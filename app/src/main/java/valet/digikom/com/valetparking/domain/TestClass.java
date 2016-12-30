4d0de466ca72ba43a1e33208d5f9469d2eadf8f3
fadli <fadlimunandar99@gmail.com>
2016-12-30 15:09:57 +0700

    create fields and getters and setters

--- app/src/main/java/valet/digikom/com/valetparking/domain/TestClass.java
+++ app/src/main/java/valet/digikom/com/valetparking/domain/TestClass.java
@@ -5,4 +5,28 @@ package valet.digikom.com.valetparking.domain;
  */
 
 public class TestClass {
+
+    private int a;
+    private int b;
+
+    public TestClass(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
 }
